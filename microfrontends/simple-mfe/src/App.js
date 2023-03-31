ebae740caa2abdf191d792303bb6e9a69ea1e922
Nathan Shaw <n.shaw@entando.com>
2023-03-31 11:41:19 -0400

    Add api claim to the config ms and update the mfe to call it

--- microfrontends/simple-mfe/src/App.js
+++ microfrontends/simple-mfe/src/App.js
@@ -1,18 +1,40 @@
+import {useState} from "react";
 import './App.css';
 
-function App({config}) {
-  const { params } = config || {};
-  const { name } = params || {};
+const API_PATH = '/api/example'
 
-  return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Hello, {name}!
-          </p>
-        </header>
-      </div>
-  );
+function App({config}) {
+    const {systemParams} = config || {};
+    const { api } = systemParams || {};
+    const url = api && api["simple-api"].url
+
+    const [payload, setPayload] = useState("")
+
+    async function callTheApi() {
+        try {
+            const apiResponse = await fetch(url + API_PATH);
+
+            if (apiResponse.ok) {
+                const apiJson = await apiResponse.json();
+                setPayload(<>{apiJson.payload}</>);
+            } else {
+                setPayload('Server responded with an error');
+            }
+        } catch (error) {
+            setPayload(error.message);
+        }
+    }
+
+    return (
+        <div className="App">
+            <div>
+                <button onClick={callTheApi}>Call the Simple API</button>
+            </div>
+            <div>
+                <span>{payload}</span>
+            </div>
+        </div>
+    );
 }
 
 export default App;
